5b5295e0deff465f97fcb173ed6d51e66d3bbef8
AndriiKom94 <andrii.kom@ukr.net>
2021-02-11 01:51:06 +0200

    First commit

---
+++ FirstAPI/cars/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from cars.serializers import CarDetailSerializer, CarListSerializer
+from cars.models import Car
+
+
+class CarCreateView(generics.CreateAPIView):
+    serializer_class = CarDetailSerializer
+
+
+class CarsListView(generics.ListAPIView):
+    serializer_class = CarListSerializer
+    queryset = Car.objects.all()
